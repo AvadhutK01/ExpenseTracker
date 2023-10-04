dffc51d5413f39701514205540fbbeca87aed3ef
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-10-04 15:54:30 +0530

    Created login page frontend

---
+++ public/LoginScript.js
@@ -0,0 +1,5 @@
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const emailInput = document.getElementById('EmailInput').value;
+    const passwordInput = document.getElementById('PasswordInput').value;
+})
