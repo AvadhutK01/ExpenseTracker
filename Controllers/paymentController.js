82a5452754b6ad091d81c0613f9736592dd08d7d
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-10-06 02:27:55 +0530

    Some  more Transaction Improvements

--- Controllers/paymentController.js
+++ Controllers/paymentController.js
@@ -2,6 +2,7 @@ const Razorpay = require("razorpay");
 const OrderData = require("../Models/paymentModel");
 const userDb = require("../Models/userModel");
 const sequelize = require("../dbConnection");
+
 module.exports.purchasePremium = async (req, res) => {
     const t = await sequelize.transaction();
     try {
@@ -24,34 +25,38 @@ module.exports.purchasePremium = async (req, res) => {
     }
 };
 
-
 module.exports.updateTransaction = async (req, res) => {
+    const t = await sequelize.transaction();
     try {
         const orderid = req.body.order_id;
         if (req.body.payment_id) {
             const { payment_id, order_id } = req.body;
-            const orderResponse = OrderData.findOne({ where: { orderid: order_id } });
-            const userUpdate = req.user.update({ isPremium: true });
+            const orderResponse = await OrderData.findOne({ where: { orderid: order_id }, transaction: t });
+            const userUpdate = await req.user.update({ isPremium: true }, { transaction: t });
             await Promise.all([orderResponse, userUpdate]);
-            const response = await OrderData.findOne({ where: { orderid: order_id } });
-            await response.update({ paymentid: payment_id, status: 'SUCCESSFUL' });
+            const response = await OrderData.findOne({ where: { orderid: order_id }, transaction: t });
+            await response.update({ paymentid: payment_id, status: 'SUCCESSFUL' }, { transaction: t });
+            await t.commit();
             return res.status(202).json({ success: true, message: "TRANSACTION SUCCESSFUL" });
         } else {
             const response = await OrderData.findOne({
                 where: {
                     orderid: orderid
                 }
-            });
-            await response.update({ status: 'FAILED' });
+            }, { transaction: t });
+            await response.update({ status: 'FAILED' }, { transaction: t });
+            await t.commit();
             return res.status(401).json({ success: false, message: "TRANSACTION FAILED" });
         }
     } catch (error) {
+        await t.rollback();
         console.error(error);
         return res.status(500).json({ success: false, message: "Internal Server Error" });
     }
-}
+};
 
 module.exports.checkPremium = async (req, res) => {
+    const t = await sequelize.transaction();
     try {
         const userid = req.user.id;
         const premiumCheck = await userDb.findOne({
@@ -59,16 +64,16 @@ module.exports.checkPremium = async (req, res) => {
                 id: userid,
                 isPremium: 1
             }
-        });
+        }, { transaction: t });
+        await t.commit();
         if (premiumCheck) {
             res.json({ result: 'true' });
-        }
-        else {
-            res.json({ result: 'false' });;
+        } else {
+            res.json({ result: 'false' });
         }
     } catch (error) {
-        res.status(500);
+        await t.rollback();
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
     }
-
-}
-
+};
